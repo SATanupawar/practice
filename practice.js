99b0079bb63f65791434ac34cbc8e8264007a73b
Vikram <vikramkunwar1404@gmail.com>
2022-09-06 13:17:59 +0530

    intial commit

---
+++ practice.js
@@ -0,0 +1,28 @@
+
+
+
+function text(){
+
+let i = document.getElementById("demo1").value.toLowerCase()
+
+document.getElementById("p_tag").innerHTML = i.split(".").pop()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
